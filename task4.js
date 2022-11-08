0307aa016d100d88fb20afd2567f845b41fdb4b7
cowtipping <turbogerbil@gmail.com>
2022-11-08 12:30:25 +0000

    Task 4 mostly done

--- task4.js
+++ task4.js
@@ -1,3 +1,15 @@
 let sevenTimesTable = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
 
 let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
+
+const result = sevenTimesTable.every(function (num) {
+    return num % 7 === 0;
+})
+
+const result2 = seventySevenTimesTable.find(function (num) {
+    return num % 77 !== 0;
+})
+
+/* const result3 = seventySevenTimesTable.findIndex(function (num){
+    return num.findIndex(461);
+}) */
