2a6d6849b9d75ced74bfd66822c9858078875414
tariba <tariqxa@gmail.com>
2022-11-09 19:50:57 +0000

    task 6 done

--- task5.js
+++ task5.js
@@ -9,3 +9,17 @@ let queue = [
   "catHorse",
   "dogPig",
 ];
+
+ let catHorse = queue.push("catHorse");
+ let removeAnimal = queue.shift("");
+let owlDonkey = queue.unshift("owlDonkey");
+let catHorseRemove= queue.splice(6,1);
+let kangarooBearRemove=queue.splice(4,1)
+const unofficialQueue = [
+  "rabbitSalmon",
+  "rabbitSalmon",
+  "rabbitSalmon",
+  "rabbitSalmon",
+];
+
+let mergeArray = queue.concat(unofficialQueue)
--- task6.js
+++ task6.js
@@ -1,11 +1,22 @@
 const broomCupboard = [2, 4, 1, 3, 7, 5, 8, 6, 10, 9, 0];
 
+function compareNumbers(a, b) {
+  return a - b;
+}
+broomCupboard.sort(compareNumbers);
+
 const potions = [
   { name: "polyjuice potion", shelf: 2 },
   { name: "draught of living death", shelf: 4 },
   { name: "pepperup potion", shelf: 1 },
   { name: "veritaserum", shelf: 3 },
 ];
+
+function compareNumbers(a, b) {
+  return b.shelf-a.shelf;
+}
+
+potions.sort(compareNumbers)
 
 const students = [
   "Cedric Diggory",
@@ -16,3 +27,17 @@ const students = [
   "Luna Lovegood",
   "Gregory Goyle",
 ];
+
+
+const compare = (a, b) => {
+  const splitA = a.split(" ");
+  const splitB = b.split(" ");
+  const lastA = splitA[splitA.length - 1];
+  const lastB = splitB[splitB.length - 1];
+
+  return lastA === lastB ?
+    compareStrings(splitA[0], splitB[0]) :
+    compareStrings(lastA, lastB);
+}
+
+console.log(students.sort(compare));
